b3d29565203b08fd77c66a844b9310c7d268c0e6
huangcygit <1013649574@qq.com>
2020-05-13 10:16:26 +0800

    zk

---
+++ zk-operator/src/main/java/com/my/zkoperator/ZkOperatorApplication.java
@@ -0,0 +1,13 @@
+package com.my.zkoperator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZkOperatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZkOperatorApplication.class, args);
+    }
+
+}
---
+++ zk-operator/src/test/java/com/my/zkoperator/ZkOperatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.my.zkoperator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZkOperatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
